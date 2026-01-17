cb972055542556b6ba8c6c055ef26f00b35a8684
Abhishek Jain <145051548+Abhishek171103@users.noreply.github.com>
2026-01-18 00:31:01 +0530

    Update cookie options for JWT token.

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -1,10 +1,12 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
   const options = {
+    httpOnly: true,
+    secure: true,
+    samesite: "none",
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
+    )
   };
 
   res.status(statusCode).cookie("token", token, options).json({
